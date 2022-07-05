ccc1b8e4550c6e3199f4dfafb9dbd1a7c9cebeb7
Elgilany Hassan <gellanyhassan0@gmail.com>
2022-07-05 22:42:56 +0000

    Update cisco-starter-repo.sh

--- cisco-starter-repo.sh
+++ cisco-starter-repo.sh
@@ -1,8 +1,14 @@
-wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+#!/bin/bash
+
+export NVM_DIR="$HOME/.nvm" && (
+    git clone https://github.com/nvm-sh/nvm.git "$NVM_DIR"
+    cd "$NVM_DIR"
+    git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`
+  ) && \. "$NVM_DIR/nvm.sh"
+  
 nvm install node
 node  -v
 npm install -g yarn
-yarn global add create-react-app
-#npx create-react-app sextant
+
 cd sextant
 npm start
